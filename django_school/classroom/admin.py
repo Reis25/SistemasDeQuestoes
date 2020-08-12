a66898615de77e29e90c51559903116ce6d5848a
Demetrios Reis <demetriosreis1@gmail.com>
2020-08-12 18:11:01 -0300

    version01

---
+++ django_school/classroom/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from classroom.models import *
+
+# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+admin.site.register(StudentAnswer)
---
+++ django_school/django_school/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+#from django_school import models
+
+# Register your models here.
+
+#admin.site.register(User)
+#admin.site.register(Subject)
+#admin.site.register(Quiz) 
+#admin.site.register(Question)
+#admin.site.register(Answer)
+#admin.site.register(Student)
+#admin.site.register(TakenQuiz)
+#admin.site.register(StudentAnswer)
+ 
